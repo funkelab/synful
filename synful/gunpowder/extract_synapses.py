48623e6e54092d5dfd9476b88736c05430a5cad3
juliabuhmann <juliab@ini.phys.ethz.ch>
2019-07-05 10:32:46 -0400

    Switch back to funlib cantor encoding function

--- synful/gunpowder/extract_synapses.py
+++ synful/gunpowder/extract_synapses.py
@@ -7,37 +7,12 @@ import gunpowder as gp
 import numpy as np
 from gunpowder import BatchFilter
 from gunpowder.contrib.points import PreSynPoint, PostSynPoint
+from funlib.math import cantor_number
 
 from .. import detection, synapse, database
 
 logger = logging.getLogger(__name__)
 
-
-def cantor_number(coordinate):
-    '''Return the "Cantor number" of tuples of arbitrary length.
-    The Cantor number is the generalization of Cantor's mapping of rational
-    numbers (2-tuples) to natural numbers for arbitrary dimensions.
-    The numbers obtained this way can be used to uniquely index a positive
-    integer coordinate in a volume of unknown size.
-    '''
-
-    if len(coordinate) == 1:
-        return coordinate[0] + 1
-
-    return (
-        pyramide_volume(len(coordinate), sum(coordinate) - 1) +
-        cantor_number(coordinate[:-1]))
-
-
-def pyramide_volume(dims, edge_length):
-
-    prod = 1
-    norm = 1
-    for d in range(dims):
-        prod *= edge_length + d + 1
-        norm *= d + 1
-
-    return prod//norm
 
 class ExtractSynapses(BatchFilter):
     '''Extract synaptic partners from 2 prediction channels. One prediction map
