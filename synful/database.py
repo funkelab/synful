b6651695381088785bcc551687dfdd00905aa1fd
juliabuhmann <juliab@ini.phys.ethz.ch>
2019-10-17 03:31:40 -0400

    Increase verbosity for printing fscore during eval

--- synful/database.py
+++ synful/database.py
@@ -631,7 +631,7 @@ class SynapseDatabase(object):
                 syn_dic['post_node_id'] = int(syn.node_id_post)
             db_list.append(syn_dic)
 
-        write_size = 1000
+        write_size = 500
         for i in range(0, len(db_list), write_size):
             self.synapses.insert_many(db_list[i:i+write_size])
         logger.debug("Insert %d synapses" % len(synapses))
